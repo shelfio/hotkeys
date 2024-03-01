5b0c4530e90a8d52f61591612ba720b0bf4c58dd
Yaroslav Chapelskyi <chapelskyi.slavik@gmail.com>
2024-03-01 08:40:11 +0200

    test: decrease size limit

--- .size-limit.cjs
+++
@@ -1,4 +0,0 @@
-module.exports = [
-  {name: 'main', path: 'lib/index.js', limit: '1.1 kb'},
-  {name: 'Hotkey', import: '{Hotkey}', path: 'lib/index.js', limit: '865 B'},
-];
---
+++ .size-limit.js
@@ -0,0 +1,4 @@
+export default [
+  {name: 'main', path: 'lib/index.js', limit: '910 B'},
+  {name: 'Hotkey', import: '{Hotkey}', path: 'lib/index.js', limit: '750 B'},
+];
